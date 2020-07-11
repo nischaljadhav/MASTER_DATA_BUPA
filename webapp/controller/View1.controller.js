49c43deb8a367bc60b6f46e5a991472609958b56
nischaljadhav <nischal.jadhav@incture.com>
2020-07-11 08:26:05 +0000

    Basic Data validation is provided.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,7 +6,6 @@ sap.ui.define([
 	"sap/m/MessageBox"
 ], function (Controller, JSONModel, MessageToast, Fragment, MessageBox) {
 	"use strict";
-	var validationFlag = true;
 
 	return Controller.extend("com.incture.ZINKTHON_SAMPLE2.controller.View1", {
 		onInit: function () {
@@ -18,27 +17,83 @@ sap.ui.define([
 			var regex = /^[a-zA-Z ]{2,20}$/;
 			var _self = this;
 			var sInput = _self.getView().byId("firstName");
-			if (sInput.getValue().match(regex)) {
+			if (sInput.getValue().match(regex) && sInput.getValue() !== "") {
 				sInput.setValueState("None");
-				this.validationFlag = true;
+				this.fNameValid = true;
 			} else {
 				sInput.setValueState("Error");
-				this.validationFlag = false;
+				this.fNameValid = false;
 			}
 		},
 		onLastNameChange: function () {
 			var regex = /^[a-zA-Z ]{2,20}$/;
 			var _self = this;
 			var sInput = _self.getView().byId("lastName");
-			if (sInput.getValue().match(regex)) {
+			if (sInput.getValue().match(regex) && sInput.getValue() !== "") {
 				sInput.setValueState("None");
-				this.validationFlag = true;
+				this.lNameValid = true;
 			} else {
 				sInput.setValueState("Error");
-				this.validationFlag = false;
+				this.lNameValid = false;
 			}
 		},
-
+		onSearchTerm1Change: function () {
+			var _self = this;
+			var sInput = _self.getView().byId("searchTerm1");
+			if (sInput.getValue() !== "") {
+				sInput.setValueState("None");
+				this.st1Valid = true;
+			} else {
+				sInput.setValueState("Error");
+				this.st1Valid = false;
+			}
+		},
+		onSearchTerm2Change: function () {
+			var _self = this;
+			var sInput = _self.getView().byId("searchTerm2");
+			if (sInput.getValue() !== "") {
+				sInput.setValueState("None");
+				this.st2Valid = true;
+			} else {
+				sInput.setValueState("Error");
+				this.st2Valid = false;
+			}
+		},
+		onLanguageKeyChange: function () {
+			var _self = this;
+			var sInput = _self.getView().byId("languageKey");
+			if (sInput.getValue() !== "") {
+				sInput.setValueState("None");
+				this.languageKeyValid = true;
+			} else {
+				sInput.setValueState("Error");
+				this.languageKeyValid = false;
+			}
+		},
+		onBPRoleChange: function () {
+			var _self = this;
+			var sInput = _self.getView().byId("bpRole");
+			if (sInput.getValue() == "Customer" || sInput.getValue() == "Vendor") {
+				sInput.setValueState("None");
+				this.bpRoleValid = true;
+			} else {
+				sInput.setValueState("Error");
+				this.bpRoleValid = false;
+			}
+		},
+		onSubmit: function () {
+			this.onFirstNameChange();
+			this.onLastNameChange();
+			this.onSearchTerm1Change();
+			this.onSearchTerm2Change();
+			this.onLanguageKeyChange();
+			this.onBPRoleChange();
+			if (this.fNameValid && this.lNameValid && this.st1Valid && this.st2Valid && this.languageKeyValid && this.bpRoleValid) {
+				MessageToast.show("Validation Successful");
+			} else {
+				MessageToast.show("Validation Failed!!");
+			}
+		},
 		onAddRow: function () {
 			var aObject = {
 				Street: "",
@@ -104,67 +159,72 @@ sap.ui.define([
 		},
 
 		fnCallbackConfirm: function (oAction) {
-			if (oAction === "YES") {
-				var index = 0;
-				var oCreatebpModel = this.getView().getModel("oCreatebpModel");
-				var oBasicData = oCreatebpModel.getProperty("/oBasicData");
-				var oCommunicationDetails = oCreatebpModel.getProperty("/oCommunicationDetails");
-				var oIdentification = oCreatebpModel.getProperty("/oIdentification");
-				var aAddressList = oCreatebpModel.getProperty("/aAddressList");
-				var aPaymentTransactions = oCreatebpModel.getProperty("/aPaymentTransactions");
-				for (index in oBasicData) {
-					oBasicData[index] = "";
+				if (oAction === "YES") {
+					var index = 0;
+					var oCreatebpModel = this.getView().getModel("oCreatebpModel");
+					var oBasicData = oCreatebpModel.getProperty("/oBasicData");
+					var oCommunicationDetails = oCreatebpModel.getProperty("/oCommunicationDetails");
+					var oIdentification = oCreatebpModel.getProperty("/oIdentification");
+					var aAddressList = oCreatebpModel.getProperty("/aAddressList");
+					var aPaymentTransactions = oCreatebpModel.getProperty("/aPaymentTransactions");
+					for (index in oBasicData) {
+						oBasicData[index] = "";
 
-				}
-				for (index in oCommunicationDetails) {
-					oCommunicationDetails[index] = "";
+					}
+					for (index in oCommunicationDetails) {
+						oCommunicationDetails[index] = "";
 
-				}
-				for (index in oIdentification) {
-					oIdentification[index] = "";
+					}
+					for (index in oIdentification) {
+						oIdentification[index] = "";
 
-				}
-				aAddressList = [];
-				aPaymentTransactions = [];
-				oCreatebpModel.setProperty("/oBasicData", oBasicData);
-				oCreatebpModel.setProperty("/oCommunicationDetails", oCommunicationDetails);
-				oCreatebpModel.setProperty("/oIdentification", oIdentification);
-				oCreatebpModel.setProperty("/aAddressList", aAddressList);
-				oCreatebpModel.setProperty("/aPaymentTransactions", aPaymentTransactions);
-				console.log(oCreatebpModel);
+					}
+					aAddressList = [];
+					aPaymentTransactions = [];
+					oCreatebpModel.setProperty("/oBasicData", oBasicData);
+					oCreatebpModel.setProperty("/oCommunicationDetails", oCommunicationDetails);
+					oCreatebpModel.setProperty("/oIdentification", oIdentification);
+					oCreatebpModel.setProperty("/aAddressList", aAddressList);
+					oCreatebpModel.setProperty("/aPaymentTransactions", aPaymentTransactions);
+					console.log(oCreatebpModel);
 
-			} else {
-				return false;
-			}
-		},
-		onSubmit: function (oEvent) {
-			var requiredInputs = this.returnIdListOfRequiredFields();
-			var passedValidation = this.validateForm(requiredInputs);
-			if (passedValidation === true && this.validationFlag === true) {
-				//show an error message, rest of code will not execute.
-				MessageToast.show("Validation Successful");
-			} else {
-				MessageToast.show("Validation Failed");
-			}
-		},
-		returnIdListOfRequiredFields: function () {
-			var requiredInputs = ["firstName", "lastName", "searchTerm1", "searchTerm2", "languageKey", "bpRole"];
-			return requiredInputs;
-		},
-		validateForm: function (requiredInputs) {
-			var _self = this;
-			var valid = true;
-			requiredInputs.forEach(function (input) {
-				var sInput = _self.getView().byId(input);
-				if (sInput.getValue() == "" || sInput.getValue() == undefined) {
-					valid = false;
-					sInput.setValueState("Error");
 				} else {
-					sInput.setValueState("None");
+					return false;
 				}
-			});
-			return valid;
-		}
+			}
+			// onSubmit: function (oEvent) {
+			// 	var requiredInputs = this.returnIdListOfRequiredFields();
+			// 	var passedValidation = this.validateForm(requiredInputs);
+			// 	if (passedValidation === true && this.fNameValid === true && this.lNameValid == true) {
+			// 		//show an error message, rest of code will not execute.
+			// 		MessageToast.show("Validation Successful");
+			// 	} else {
+			// 		MessageToast.show("Validation Failed");
+			// 	}
+			// },
+			// returnIdListOfRequiredFields: function () {
+			// 	var requiredInputs = ["firstName", "lastName", "searchTerm1", "searchTerm2", "languageKey", "bpRole"];
+			// 	return requiredInputs;
+			// },
+			// validateForm: function (requiredInputs) {
+			// 	var _self = this;
+			// 	var valid = true;
+			// 	requiredInputs.forEach(function (input) {
+			// 		var sInput = _self.getView().byId(input);
+			// 		var previousState = _self.getView().byId(input).getValueState();
+			// 		if (sInput.getValue() == "" || sInput.getValue() == undefined) {
+			// 			valid = false;
+			// 			sInput.setValueState("Error");
+			// 		} else {
+			// 			if (previousState === "None") {
+			// 				sInput.setValueState("None");
+			// 			} else {
+			// 				sInput.setValueState("Error");
+			// 			}
+			// 		}
+			// 	});
+			// 	return valid;
+			// }
 
 	});
 });
